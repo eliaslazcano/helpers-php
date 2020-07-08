ff8775fb920d3083de63c9914150307b718a849e
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-07-07 22:45:37 -0400

    Update JwtHelper.php
    Retrocompatibilidade com o PHP 5.

--- JwtHelper.php
+++ JwtHelper.php
@@ -19,14 +19,14 @@ class JwtHelper
    * @param string $segredo Uma chave, usada para criptografar o token. Se não for passada, será gerada aleatoriamente.
    * @param array $dados Array de dados que o token carregará, podendo ser consultado abertamente.
    */
-  public function __construct(string $segredo = null, array $dados = [])
+  public function __construct($segredo = null, $dados = array())
   {
     if (!$segredo) $segredo = md5(uniqid(mt_rand().mt_rand(), true), false);
 
-    $header = [
+    $header = array(
       'alg' => 'HS256', //algoritmo de criptografia
       'typ' => 'JWT'    //tipo de token
-    ];
+    );
     $header = json_encode($header);         //converte em string JSON
     $header = base64_encode($header);       //codifica em texto BASE64
 
@@ -49,7 +49,7 @@ class JwtHelper
    * Obtem o token padronizado em string JWT
    * @return string
    */
-  public function getToken(): string
+  public function getToken()
   {
     return $this->token;
   }
@@ -58,7 +58,7 @@ class JwtHelper
    * Obtem a chave secreta capaz de autenticar o token
    * @return string
    */
-  public function getSegredo(): string
+  public function getSegredo()
   {
     return $this->segredo;
   }
@@ -67,7 +67,7 @@ class JwtHelper
    * Obtem um array com os dados contidos no Token
    * @return array
    */
-  public function getDados(): array
+  public function getDados()
   {
     $part = explode(".", $this->token);
     $payload = $part[1];
@@ -81,7 +81,7 @@ class JwtHelper
    * @param string $segredo A chave que foi utilizada na criação do token
    * @return bool
    */
-  public function validarSegredo(string $segredo): bool
+  public function validarSegredo($segredo)
   {
     $part = explode(".", $this->token);
     $header = $part[0];
@@ -101,14 +101,14 @@ class JwtHelper
    * @param array $dados Dados incorporados ao token. Evite dados sigilosos como senhas.
    * @return array|string Se voce nao fornecer um segredo, o retorno sera um array com os indices 'token' e 'segredo'
    */
-  public static function criarToken(string $segredo = null, array $dados = [])
+  public static function criarToken($segredo = null, $dados = array())
   {
     $key = $segredo ? $segredo : md5(uniqid(mt_rand().mt_rand(), true), false);
 
-    $header = [
+    $header = array(
       'alg' => 'HS256', //informa o algoritmo de criptografia
       'typ' => 'JWT'    //informa o tipo de token
-    ];
+    );
     $header = json_encode($header);         //converte em string JSON
     $header = base64_encode($header);       //codifica em texto BASE64
 
@@ -128,7 +128,7 @@ class JwtHelper
    * @param string $segredo A string que foi utilizada na criação do token para criptografar
    * @return bool
    */
-  public static function validarToken(string $token, string $segredo): bool
+  public static function validarToken($token, $segredo)
   {
     $part = explode(".", $token);
     $header = $part[0];
@@ -146,7 +146,7 @@ class JwtHelper
    * @param string $token O token em string JWT
    * @return object
    */
-  public static function obterDados(string $token)
+  public static function obterDados($token)
   {
     $part = explode(".", $token);
     $payload = $part[1];
